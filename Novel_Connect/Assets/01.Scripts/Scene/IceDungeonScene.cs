d6f3e3cfebf8c59b7e8d4b98b6b23f6067243b20
yumin01 <yumin01@g.shingu.ac.kr>
2023-10-10 12:16:51 +0900

    B1_Add_Background

--- Novel_Connect/Assets/01.Scripts/Scene/IceDungeonScene.cs
+++ Novel_Connect/Assets/01.Scripts/Scene/IceDungeonScene.cs
@@ -7,6 +7,9 @@ public class IceDungeonScene : BaseScene
     public override void Init()
     {
         Debug.Log("아이스 던전 켜짐");
+        Managers.Object.Player.Init(1, "Normal");
+        Managers.Object.Player.SetPosition(new Vector3(-3.34899998f, -0.9f, 0));
+        Managers.Screen.SetCameraTarget(Managers.Object.Player.trans);
     }
 
     public override void Clear()
